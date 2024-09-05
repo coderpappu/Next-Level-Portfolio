267abfe3678ccdc96c160b22625f4dcaabf3228b
coderpappu <pappudey.coder@gmail.com>
2024-09-06 02:07:47 +0600

    initial setup completed and menu bar created

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./Home";
+
+const Layout = () => {
+  return (
+    <div className="wrapper-container">
+      <Home />
+    </div>
+  );
+};
+
+export default Layout;
