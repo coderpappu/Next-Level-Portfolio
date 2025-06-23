8dd08ce4986cad66b3e879b214ca33d89a5158b5
coderpappu <pappudey.coder@gmail.com>
2025-06-23 20:11:33 +0600

    test

---
+++ src/components/Heading.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import Logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+
+const Heading = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Toggle mobile menu visibility
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="border-b border-[#506b860f] py-5 font-poppins  w-full bg-[#0F172A] z-50">
+      <div className="xl:wrapper-container lg:px-0 px-6 flex flex-wrap items-center justify-between text-[#e0e0e0]">
+        {/* Logo */}
+        <div>
+          <img src={Logo} alt="pappudey Logo" className="w-8 h-8" />
+        </div>
+
+        {/* Desktop Menu */}
+        <div className="hidden md:flex items-center">
+          <ul className="flex space-x-8 font-semibold">
+            {["Home", "Services", "Work", "Resume", "Skills", "Contact"].map(
+              (item, idx) => (
+                <li key={idx} className="relative group">
+                  <Link
+                    to={`/${item.toLowerCase()}`}
+                    className="relative group"
+                  >
+                    {item}
+                    <span className="top-6 absolute left-0 bottom-0 w-full h-[1px] bg-gradient-to-r from-[#38BDF8] to-[#0F172A] scale-x-0 group-hover:scale-x-100 transform origin-left transition-transform duration-300 ease-in-out"></span>
+                  </Link>
+                </li>
+              )
+            )}
+          </ul>
+          <div className="w-36 text-center">
+            <button className="buttonStyle px-4">Hire Me</button>
+          </div>
+        </div>
+
+        {/* Mobile Menu Button */}
+
+        <div className="md:hidden flex items-center">
+          <button
+            className="text-white focus:outline-none"
+            onClick={toggleMenu}
+          >
+            {/* Hamburger icon */}
+            <svg
+              className="w-8 h-8"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } md:hidden bg-[#0F172A] py-5 px-8`}
+      >
+        <ul className="space-y-4 text-center font-semibold">
+          {["Home", "Services", "Work", "Resume", "Skills", "Contact"].map(
+            (item, idx) => (
+              <li key={idx}>
+                <Link
+                  to={`/${item.toLowerCase()}`}
+                  className="block py-2 hover:text-[#38BDF8] text-[#ddd]"
+                  onClick={toggleMenu}
+                >
+                  {item}
+                </Link>
+              </li>
+            )
+          )}
+        </ul>
+        <div className="text-center mt-4">
+          <button className="bg-gradient-to-br from-violet-950 via-violet-900 to-violet-950 px-4">
+            Hire Me
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Heading;
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+// Assuming you'll add the CSS here
+import { BiLogoLinkedin } from "react-icons/bi";
+import { IoLogoGithub } from "react-icons/io";
+import { RiFacebookFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import pappuImg from "../assets/pappu-dey.jpg";
+
+const HeroSection = () => {
+  return (
+    <div className="relative h-auto hero-section pt-[60px] md:pt-[100px] pb-16 z-50 px-4 sm:px-6 lg:px-0">
+      <div className="intro_text">
+        {/* SVG for animated "Hi" text */}
+        <svg viewBox="0 0 500 500">
+          <text x="50%" y="50%" textAnchor="middle" className="animate-stroke ">
+            Hi
+          </text>
+        </svg>
+      </div>
+      <div className="flex flex-col lg:flex-row justify-between font-sora">
+        {/* Left side */}
+        <div className="w-full lg:w-[56%] mb-8 sm:mb-12 lg:mb-0">
+          <h2 className="text-3xl sm:text-4xl lg:text-[36px] mb-3 lg:mb-5 font-bold text-white">
+            I am Pappu
+          </h2>
+          <h2
+            className="text-4xl sm:text-5xl md:text-[50px] lg:text-[50px] xl:text-[65px] font-bold text-transparent bg-clip-text leading-[1.4] sm:leading-[1.3] lg:leading-[1.3] inline-block"
+            style={{
+              backgroundImage:
+                "linear-gradient(90deg, rgb(14, 165, 234), rgb(13, 209, 209) 51%, rgb(14, 165, 234))",
+            }}
+          >
+            Software Engineer +
+          </h2>
+          <h2
+            className="text-4xl sm:text-5xl  md:text-[50px] lg:text-[50px] xl:text-[65px]  font-bold text-transparent bg-clip-text leading-[1.4] sm:leading-[1.3] lg:leading-[1.3]"
+            style={{
+              backgroundImage:
+                "linear-gradient(90deg, rgb(14, 165, 234), rgb(13, 209, 209) 51%, rgb(14, 165, 234))",
+            }}
+          >
+            UX Designer
+          </h2>
+          <p className="mt-4 sm:mt-6 lg:mt-10 text-base sm:text-lg lg:text-lg text-[#ffffffb0] max-w-full sm:w-[90%] md:max-w-[70%]">
+            I break down complex user experience problems to create integrity
+            focused solutions that connect billions of people
+          </p>
+          <div className="mt-8 sm:mt-6 lg:mt-10 flex flex-col sm:justify-center md:justify-start sm:flex-row items-center gap-4">
+            <button className="px-6 sm:px-8 lg:px-10 py-3 sm:py-4 border border-[#0EABE6] hover:bg-[#0DCFD2] text-[#0EABE6] rounded-full cursor-pointer transition duration-300 ease-in-out">
+              Download CV
+            </button>
+            {/* Social media icons */}
+            <ul className="flex space-x-4">
+              <li className="relative w-8 sm:w-9 h-8 sm:h-9 rounded-full border border-[#0EABE6] text-[#0EABE6] flex items-center justify-center overflow-hidden group">
+                <Link
+                  to="#"
+                  className="relative z-10 text-[#0EABE6] group-hover:text-white transition duration-300"
+                >
+                  <RiFacebookFill size={20} />
+                </Link>
+                <span className="absolute inset-0 bg-[#0EABE6] rounded-full transform scale-0 group-hover:scale-100 transition-transform duration-300"></span>
+              </li>
+              <li className="relative w-8 sm:w-9 h-8 sm:h-9 rounded-full border border-[#0EABE6] text-[#0EABE6] flex items-center justify-center overflow-hidden group">
+                <Link
+                  to="#"
+                  className="relative z-10 text-[#0EABE6] group-hover:text-white transition duration-300"
+                >
+                  <BiLogoLinkedin size={20} />
+                </Link>
+                <span className="absolute inset-0 bg-[#0EABE6] rounded-full transform scale-0 group-hover:scale-100 transition-transform duration-300"></span>
+              </li>
+              <li className="relative w-8 sm:w-9 h-8 sm:h-9 rounded-full border border-[#0EABE6] text-[#0EABE6] flex items-center justify-center overflow-hidden group">
+                <Link
+                  to="#"
+                  className="relative z-10 text-[#0EABE6] group-hover:text-white transition duration-300"
+                >
+                  <IoLogoGithub size={20} />
+                </Link>
+                <span className="absolute inset-0 bg-[#0EABE6] rounded-full transform scale-0 group-hover:scale-100 transition-transform duration-300"></span>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Right side */}
+        <div className=" max-w-[280px] sm:max-w-sm md:max-w-md lg:w-[44%] w-full mx-auto hero-image-box z-30">
+          <img
+            src={pappuImg}
+            alt="pappudey_picture"
+            className="rounded-[38px] border-2 border-[#7343D2] transform rotate-[4.29deg] transition-transform duration-300 ease-in-out hover:rotate-0 hover:border-[#0DCFD2] w-full h-auto"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
